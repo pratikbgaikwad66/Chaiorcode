a16fa4d960eee88fe586875e4f3fed8ab2d5cb09
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2023-08-25 10:28:24 +0000

    coneverstion

---
+++ basicsjs/03converstionoperatation.js
@@ -0,0 +1,21 @@
+let score = ""
+
+// console.log(typeof scroe);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber );
+
+//"33" => 33
+//"33abc" => nan
+//true => 1; false=> 0 
+
+let isLoggedIn = 1345445
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
