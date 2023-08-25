3d44bdd82118bfa1ecc44d535695dea6b2438d8a
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2023-08-25 09:49:35 +0000

    datatype

---
+++ basicsjs/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14067
+let accountEmail = "pratik@gmail.com"
+var accountpassword = "12344"
+accountCity = "jaipur"
+
+console.table( [accountId,accountEmail,accountpassword,accountCity] )
---
+++ basicsjs/datatypes1.js
@@ -0,0 +1,15 @@
+//number => 2 to power 53 
+//bigint
+//string =>""
+//boolean => true/false
+//null =>standalone value 
+//undifined 
+//symbol =>unique
+
+
+//object
+
+console.log( typeof undefined);  //undifined
+console.log( typeof null); //object
+const accountId = "32424"
+console.log( accountId );
