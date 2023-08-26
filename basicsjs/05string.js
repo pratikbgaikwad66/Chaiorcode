b69de84f710f9e545f2ca637b9ca9c068b959a10
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2023-08-26 09:38:40 +0000

    string and maths

---
+++ basicsjs/05string.js
@@ -0,0 +1,32 @@
+const name ="pratik"
+const repoCount = 50 
+
+//console.log(name + repoCunt is $);
+
+console.log(`hello may name is ${name} and my repo count id ${repoCount}`);
+
+const gameName = new String('pratik')
+// console.log(gameName[6]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+//  console.log(gameName.charAt(3));
+//  console.log(gameName.indexOf('t'));
+
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+
+
+ const anotherString = gameName.slice(-2.5)
+ console.log(anotherString);
+
+ const newStringOne = "    pratik   "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+ 
+
+ const url = "https://pratik.com/pratik%20gaikwad"
+ console.log(url.replace('%20', '_'))
+
+ console.log(url.includes('pratik'))
+
+ 
---
+++ basicsjs/datatype-summry.js
@@ -0,0 +1,16 @@
+//primitive data type
+// 7 types > string ,number , boolean ,null ,undefinded , symbol, Bigint 
+
+const score = 100 
+const scoreValue =100.3
+
+// //myfunction 
+// const myFunction = function(){
+//     consol.log("hello word")
+// }
+const accountId = 14067
+let accountEmail = "pratik@gmail.com"
+var accountpassword = "12344"
+accountCity = "jaipur"
+
+console.table( [accountId,accountEmail,accountpassword,accountCity] )
