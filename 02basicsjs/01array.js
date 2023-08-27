3bd0066e1fab450803c12e49a3590ccb31721d66
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2023-08-27 09:18:28 +0000

    array

---
+++ 02basicsjs/01array.js
@@ -0,0 +1,36 @@
+const myarr = [0 ,1 ,2 ,3 ,4, 5, ]
+const myhero = [ "spider ", "shakti"]
+
+const myarr2 = new Array(1, 2, 3, 4)
+console.log(myarr[1]);
+
+// array methodes
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.shift()
+// console.log(myarr)
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf());
+
+// const newArr = myarr.join()
+
+// console.log(typeof myarr);
+// console.log(typeof newArr);
+
+console.log("a", myarr);
+
+const myn1 = myarr.slice(1, 3)
+
+console.log(myn1);
+console.log("B",myarr);
+
+
+const myn2 = myarr.splice(1, 3)
+console.log("c",myarr);
+console.log(myn2);
+
--- basicsjs/datesin.js
+++ basicsjs/datesin.js
@@ -1,4 +1,4 @@
-let myDate = new Date()
+// let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toISOString());
 // console.log(myDate.toDateString());
@@ -10,10 +10,16 @@ let myDate = new Date()
 // let myCreatedDate = new Date( 2023, 0, 23, 5 , 3)
 // console.log(myCreatedDate.toLocaleString());
 
-let newDate = new Date()
-console.log(newDate.getMonth( + 1));
-// console.log(newDate.getDay());
 
-newDate.toLocaleString('default', {
-    weekday:"long"
-})
+// console.log(newDate.getMonth( + 1));
+// // console.log(newDate.getDay());
+
+// newDate.toLocaleString('default', {
+//     weekday:"long"
+// })
+
+let newDate =new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getMonth() + 1);
