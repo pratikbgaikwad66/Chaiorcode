766f08484a22ea600cdf0d21306ad40eddc80982
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2023-08-27 11:25:43 +0000

    objects

---
+++ 02basicsjs/04objects.js
@@ -0,0 +1,64 @@
+// const tinderUser = new object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedin = "false"
+
+// console.log(tinderUser);
+
+const regularuser = {
+
+    email : "pratik@gamil.com",
+    fullname : {
+        userfullname :{ 
+            firstname :"pratik",
+            lastname : "gaikwad"
+    
+    
+    }
+
+    }
+}
+// console.log(regularuser.fullname. userfullname. firstname. lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const user = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    
+    
+]
+
+user[1].email
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
