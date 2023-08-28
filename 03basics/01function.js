55e075fd3115713712dc769e595de613e3ca1ed9
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2023-08-28 11:58:10 +0000

    function

--- 03basics/01function.js
+++ 03basics/01function.js
@@ -36,3 +36,30 @@ return `${username} justloggedin`
 }
 // console.log(loginUserMessage("pratik"))
 console.log(loginUserMessage("pratik"))
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 600, 700, 800))
+
+const youser = {
+    username: "hitesh",
+    price: 129 
+}
+function handleObject(anayobject){
+    console.log(`username is ${anayobject.username} and price is ${anayobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+
+})
+
+const myNewArray = [100, 200, 200, 300, ]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));1
